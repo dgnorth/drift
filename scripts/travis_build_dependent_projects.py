658a2fba66edc74a7f9fcf7f9f07d2b53a3bd087
Matthias Gudmundsson <mattigumm@gmail.com>
2017-04-26 17:33:04 +0000

    Debugging travis script. Work in progres..

--- scripts/travis_build_dependent_projects.py
+++ scripts/travis_build_dependent_projects.py
@@ -10,12 +10,17 @@ def main():
     conn = TravisPy.github_auth(os.environ['GITHUB_KEY'], travispy.PRIVATE)
     repos = conn.repos()
     for repo in repos:
-        build = conn.build(repo.last_build_id)
-        if "kitrun.py" in build.config.get("script", [""])[0]:
-            print "Found drift project: ", repo.slug
-            if not build.running:
-                print "Restarting..."
-                build.restart()
+        print "Repo:", repo
+        print repo.last_build_id, repo.description, repo.last_build_number, repo.active
+        try:
+            build = conn.build(repo.last_build_id)
+            if "kitrun.py" in build.config.get("script", [""])[0]:
+                print "Found drift project: ", repo.slug
+                if not build.running:
+                    print "Restarting..."
+                    build.restart()
+        except Exception as e:
+            print "Can't check repo: ", e
 
 
 if __name__ == "__main__":
