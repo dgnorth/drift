e96745d0529f9422f338a7e1fb2ee535d3e1b3f5
Matthias Gudmundsson <mattigumm@gmail.com>
2017-04-27 11:45:50 +0000

    Updated travis trigger script to support both public and private repos. More info is spewed out to console as well.

--- scripts/travis_build_dependent_projects.py
+++ scripts/travis_build_dependent_projects.py
@@ -8,11 +8,18 @@ from travispy import TravisPy
 
 def main():
 
+    restarted = []
+    building = []
+
     for domain in [travispy.PUBLIC, travispy.PRIVATE]:
+        print "Enumerate repos on ", domain
         conn = TravisPy.github_auth(os.environ['GITHUB_KEY'], domain)
-        repos = conn.repos()
+        user = conn.user()
+        repos = conn.repos(member=user.login)
         for repo in repos:
-            print "Repo:", repo.slug, repo.last_build_id, repo.description, repo.last_build_number, repo.active, domain
+            if not repo.active:
+                continue
+            print u"Checking repo: {}\n{}".format(repo.slug, repo.description)
             try:
                 build = conn.build(repo.last_build_id)
                 if "kitrun.py" in build.config.get("script", [""])[0]:
@@ -20,10 +27,30 @@ def main():
                     if not build.running:
                         print "Restarting..."
                         build.restart()
+                        restarted.append(repo.slug)
+                    else:
+                        print "Build is already running!"
+                        building.append(repo.slug)
+                else:
+                    print "Not a drift based project."
             except Exception as e:
                 print "Can't build repo: ", e
 
             print ""
+
+        if restarted:
+            print "Repos restarted:"
+            for reponame in restarted:
+                print "\t", reponame
+        else:
+            print "No builds restarted."
+
+        if building:
+            print "Repos already building:"
+            for reponame in building:
+                print "\t", reponame
+
+
 
 
 if __name__ == "__main__":
