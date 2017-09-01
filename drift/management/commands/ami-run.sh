4832b6f7b39e9144189f2b6b3be0eceb20243768
Andreas Axelsson <andreas.axelsson@gmail.com>
2017-09-01 20:08:46 +0800

    Make CloudWatch Logs installation be non-interactive

--- drift/management/commands/ami-run.sh
+++ drift/management/commands/ami-run.sh
@@ -5,5 +5,5 @@ sudo service $DRIFT_SERVICE restart
 SERVICE_PATH=$(pip show $DRIFT_SERVICE |grep "^Location:" |sed -e 's/Location: //g')
 
 curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
-sudo python awslogs-agent-setup.py --region $AWS_REGION --configfile $SERVICE_PATH/config/awslogs/awslogs.conf
+sudo python awslogs-agent-setup.py --region $AWS_REGION --non-interactive --configfile $SERVICE_PATH/config/awslogs/awslogs.conf
 sudo service awslogs restart
