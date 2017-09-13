f8cb9f9977c5eede2ca458f3077c73dbf2584739
Andreas Axelsson <axl@directivegames.com>
2017-09-13 20:17:55 +0800

    Fix ami-run and quickdeploy scripts to work with systemd

--- drift/management/commands/ami-run.sh
+++ drift/management/commands/ami-run.sh
@@ -2,9 +2,9 @@
 sudo bash -c "echo DRIFT_CONFIG_URL=$DRIFT_CONFIG_URL >> /etc/environment"
 sudo bash -c "echo DRIFT_TIER=$DRIFT_TIER >> /etc/environment"
 sudo bash -c "echo DRIFT_APP_ROOT=$DRIFT_APP_ROOT >> /etc/environment"
-####sudo service $DRIFT_SERVICE restart
+sudo systemctl restart $DRIFT_SERVICE
 
 curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -o /home/ubuntu/aws/awslogs-agent-setup.py
 CONFIG_FILE=/home/ubuntu/aws/awslogs/awslogs.conf
 sudo python /home/ubuntu/aws/awslogs-agent-setup.py --region $AWS_REGION --non-interactive --configfile $CONFIG_FILE
-####sudo service awslogs restart
+sudo systemctl restart awslogs
--- drift/management/commands/quickdeploy.sh
+++ drift/management/commands/quickdeploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # The environment variables must be set, obviously
 sudo rm -f ${UWSGI_LOGFILE}
-sudo service ${DRIFT_SERVICE_NAME} restart
-sudo service ${DRIFT_SERVICE_NAME}-celery restart
-sudo service ${DRIFT_SERVICE_NAME} status
+sudo systemctl restart ${DRIFT_SERVICE_NAME}
+sudo systemctl restart ${DRIFT_SERVICE_NAME}-celery
+sudo systemctl status ${DRIFT_SERVICE_NAME}
 
 curl http://127.0.0.1:${DRIFT_PORT} -H "Accept: application/json"
 cat ${UWSGI_LOGFILE}
