39589f5aba1965471309c0801218bb69d4a5df7f
Matthias Gudmundsson <matti@directivegames.com>
2017-10-12 15:47:00 +0000

    Make quickdeploy feedback nicer.

--- drift/management/commands/quickdeploy.sh
+++ drift/management/commands/quickdeploy.sh
@@ -4,8 +4,9 @@ sudo rm -f ${UWSGI_LOGFILE}
 sudo systemctl daemon-reload
 sudo systemctl restart ${DRIFT_SERVICE_NAME}
 sudo systemctl restart ${DRIFT_SERVICE_NAME}-celery
-sudo systemctl status ${DRIFT_SERVICE_NAME}
-sudo systemctl status ${DRIFT_SERVICE_NAME}-celery
+sudo systemctl status ${DRIFT_SERVICE_NAME} --no-pager
+sudo systemctl status ${DRIFT_SERVICE_NAME}-celery --no-pager
 
-curl http://127.0.0.1:8080 -H "Accept: application/json"
+echo "Get root endpoint:"
+curl -I -X GET http://127.0.0.1:8080 -H "Accept: application/json"
 sudo cat ${UWSGI_LOGFILE}
