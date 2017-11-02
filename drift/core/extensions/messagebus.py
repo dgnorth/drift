30b0e781c231d05ecd11b46422f328a3555c67f3
Matthias Gudmundsson <matti@directivegames.com>
2017-11-02 15:46:25 +0000

    Adding placeholder message bus.

---
+++ drift/core/extensions/messagebus.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+    Message bus - placeholder code until proper message broker is in place.
+"""
+from __future__ import absolute_import
+
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+class MessageBus(object):
+
+    def __init__(self, app=None):
+        self.app = None
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+
+        app.extensions['messagebus'] = self
+        self._consumers = {}
+
+    def register_consumer(self, callback, queue_name):
+        self._consumers.setdefault(queue_name, []).append(callback)
+
+    def publish_message(self, queue_name, message):
+        for consumer in self._consumers.get(queue_name, []):
+            consumer(queue_name, message)
+
+
+def register_extension(app):
+    app.messagebus = MessageBus(app)
