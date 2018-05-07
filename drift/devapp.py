a498c017395929c59004590940401b8d7f20f030
Matthias Gudmundsson <matti@directivegames.com>
2018-05-07 16:05:23 +0000

    Cosmetic change of name.

--- drift/devapp.py
+++ drift/devapp.py
@@ -6,7 +6,7 @@ from flaskfactory import drift_app
 
 logging.basicConfig(level='INFO')
 
-# Default tenant for devserver is 'developer'
+# Default tenant for devapp is 'developer'
 tenant_name = os.environ.setdefault('DRIFT_DEFAULT_TENANT', 'developer')
-logging.getLogger(__name__).info("Default tenant on this devserver is '%s'.", tenant_name)
+logging.getLogger(__name__).info("Default tenant for this developer app is '%s'.", tenant_name)
 app = drift_app()
