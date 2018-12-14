3ad6806c1301fc0b8a1c11efefe2e7dae8d9ad7f
Matthias Gudmundsson <matti@directivegames.com>
2018-12-14 15:34:15 +0000

    Add a deprecation warning when importing drift.urlregistry insteasd of drift.core.extensions.urlregistry

--- drift/urlregistry.py
+++ drift/urlregistry.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+import warnings
+
+warnings.warn(
+    "please import drift.core.extensions.urlregistry instead of drift.urlregistry.",
+    DeprecationWarning,
+    stacklevel=2)
 
 
 # HACK: Temporary placeholder file until billion imports have been fixed up
-
 def register_endpoints(f):
     from drift.core.extensions.urlregistry import register_endpoints as reg
     return reg(f)
