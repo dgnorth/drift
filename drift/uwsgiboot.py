7f59987e0092240aa8a1cd1e09025da4203b4dcf
Jon Bjarnason <nonnib@gmail.com>
2019-01-13 16:55:31 +0000

    Adding ddtrace to drift.

--- drift/uwsgiboot.py
+++ drift/uwsgiboot.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
 
+try:
+    import ddtrace
+    ddtrace.patch_all()
+except ImportError:
+    pass
+
 from .flaskfactory import drift_app
 
 logging.basicConfig(level='INFO')
