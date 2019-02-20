106f1638a9e52470a23e1ae7909670ebd0598ed9
Matthias Gudmundsson <matti@directivegames.com>
2019-02-20 13:27:23 +0000

    Only enable Datadog APM if environment variable ENABLE_DATADOG_APM=1

--- drift/contrib/flask/datadogapp.py
+++ drift/contrib/flask/datadogapp.py
@@ -3,10 +3,13 @@ Flask app with Datadog APM bootstrap.
 Log level is set to 'INFO'.
 """
 import logging
-import ddtrace
+import os
 
 from drift.flaskfactory import drift_app
 
+
 logging.basicConfig(level='INFO')
-ddtrace.patch_all()
+if os.environ.get('ENABLE_DATADOG_APM', '0') == '1':
+    import ddtrace
+    ddtrace.patch_all()
 app = drift_app()
