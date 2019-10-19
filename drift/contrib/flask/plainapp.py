716ad34e257cdf531721a41f590c4ef894539ccc
Jon Bjarnason <jonb@wuxinextcode.com>
2019-10-19 14:18:08 +0000

    Removing logging setup from apps

--- drift/contrib/flask/plainapp.py
+++ drift/contrib/flask/plainapp.py
@@ -1,10 +1,5 @@
 """
-Flask app.
-Log level is set to 'INFO'.
+Basic Flask app.
 """
-import logging
-
 from drift.flaskfactory import drift_app
-
-logging.basicConfig(level='INFO')
 app = drift_app()
