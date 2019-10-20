1fe61a9e356f3f916ecf9a0a832f0ed19a1e1003
Jon Bjarnason <jonb@wuxinextcode.com>
2019-10-20 14:31:10 +0000

    cleaning up logging module

--- drift/core/extensions/logging.py
+++ drift/core/extensions/logging.py
@@ -6,24 +6,18 @@
     Set up logging based on config dict.
 
 """
-from __future__ import absolute_import
 
-import os
 import logging
-from logging.handlers import SysLogHandler
 import logging.config
 import json
 import datetime
 import sys
-import time
 import uuid
-from socket import gethostname
 from collections import OrderedDict
-from functools import wraps
+from logstash_formatter import LogstashFormatterV1
 
-import six
-from six.moves.urllib.parse import urlsplit
-from flask import g, request, current_app
+from urllib.parse import urlsplit
+from flask import g, request
 import sentry_sdk
 
 from drift.core.extensions.jwt import current_user
@@ -31,15 +25,6 @@ from drift.utils import get_tier_name
 
 
 log = logging.getLogger(__name__)
-
-
-def get_stream_handler():
-    """returns a stream handler with standard formatting for use in local development"""
-    stream_handler = logging.StreamHandler()
-    stream_formatter = logging.Formatter(
-        fmt='%(asctime)s %(levelname)-8s %(name)-15s %(message)s')
-    stream_handler.setFormatter(stream_formatter)
-    return stream_handler
 
 
 def get_caller():
@@ -111,7 +96,7 @@ def get_log_details():
                 jwt_context[key] = v
             if k == "roles" and v:
                 jwt_context[k] = ",".join(v)
-    except Exception as e:
+    except Exception:
         pass
     if jwt_context:
         details["user"] = jwt_context
@@ -125,217 +110,9 @@ def get_log_details():
     return details
 
 
-# # Custom log record
-# class DriftLogRecord(logging.LogRecord):
-#     def __init__(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None, sinfo=None):
-#         if six.PY3:
-#             super(DriftLogRecord, self).__init__(name, level, fn, lno, msg, args, exc_info, func, sinfo)
-#         else:
-#             super(DriftLogRecord, self).__init__(name, level, fn, lno, msg, args, exc_info, func)
-#         log_details = get_log_details()
-#         if extra:
-#             log_details.update(extra)
-#         for k, v in log_details.items():
-#             setattr(self, k, v)
-#         logger_fields = (
-#             "levelname", "levelno", "process", "thread", "name",
-#             "filename", "module", "funcName", "lineno")
-#         for f in logger_fields:
-#             log_details["logger"][f] = getattr(self, f, None)
-#         try:
-#             correlation_id = request.correlation_id
-#         except Exception:
-#             correlation_id = None
-
-#         log_details["logger"]["correlation_id"] = correlation_id
-#         log_details["logger"]["created"] = datetime.datetime.utcnow().isoformat() + "Z"
-#         for k, v in log_details.items():
-#             setattr(self, k, v)
-
-
-# class ContextAwareLogger(logging.Logger):
-#     """
-#     The context aware logger allows the caller to specify the extra information
-#     in the following manner:
-#     log.info(message, extra={k: v})
-#     Internally, the "extra" parameter will be transformed into
-#     extra={
-#         "extra": {k, v}
-#     }
-#     This way, the extra information can easily be retrieved
-#     from the "extra" field of the log record
-#     """
-#     def _log(self, level, msg, args, exc_info=None, extra=None):
-#         if extra is not None:
-#             extra = {"extra": extra}
-#         super(ContextAwareLogger, self)._log(level, msg, args, exc_info, extra)
-
-#     def makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None, sinfo=None):
-#         return DriftLogRecord(name, level, fn, lno, msg, args, exc_info, func, extra, sinfo)
-
-
-# class JSONFormatter(logging.Formatter):
-
-#     """
-#     Format log message as JSON.
-#     """
-#     source_host = gethostname()
-#     log_tag = None
-
-#     def __init__(self):
-#         super(JSONFormatter, self).__init__()
-
-#     def formatTime(self, record, datefmt=None):
-#         dt = datetime.datetime.fromtimestamp(record.created)
-#         return dt.isoformat() + "Z"
-
-#     def get_formatted_data(self, record):
-
-#         data = OrderedDict()
-#         # put the timestamp first for splunk timestamp indexing
-#         data["timestamp"] = self.formatTime(record)
-#         if hasattr(record, "logger") and "tier" in record.logger:
-#             data["tenant"] = "{}.{}".format(record.logger.get("tier", None), record.logger.get("tenant", None))
-
-#         field_names = "logger", "client", "user"
-#         data.update({
-#             key: getattr(record, key)
-#             for key in field_names
-#             if hasattr(record, key)
-#         })
-#         return data
-
-#     def format(self, record):
-#         data = self.get_formatted_data(record)
-#         json_text = json.dumps(data, default=self._json_default)
-#         return json_text
-
-#     def json_format(self, data):
-#         json_text = json.dumps(data, default=self._json_default)
-#         return "drift.%s: @cee: %s" % (self.log_tag, json_text)
-
-#     @staticmethod
-#     def _json_default(obj):
-#         """
-#         Coerce everything to strings.
-#         All objects representing time get output as ISO8601.
-#         """
-#         if isinstance(obj, datetime.datetime) or \
-#            isinstance(obj, datetime.date) or \
-#            isinstance(obj, datetime.time):
-#             return obj.isoformat()
-#         else:
-#             return str(obj)
-
-
-# class ServerLogFormatter(JSONFormatter):
-#     log_tag = "server"
-
-#     def format(self, record):
-#         data = self.get_formatted_data(record)
-#         data["message"] = super(JSONFormatter, self).format(record)
-#         data["level"] = record.levelname
-#         try:
-#             data["request"] = "{} {}".format(request.method, request.url)
-#         except Exception:
-#             pass
-#         return self.json_format(data)
-
-
-# class EventLogFormatter(JSONFormatter):
-#     log_tag = "events"
-
-#     def format(self, record):
-#         data = self.get_formatted_data(record)
-#         data["event_name"] = super(JSONFormatter, self).format(record)
-#         data.update(getattr(record, "extra", {}))
-#         return self.json_format(data)
-
-
-# class ClientLogFormatter(JSONFormatter):
-#     log_tag = "client"
-
-#     def format(self, record):
-#         data = self.get_formatted_data(record)
-#         data.update(getattr(record, "extra", {}))
-#         return self.json_format(data)
-
-
-# def trim_logger(data):
-#     # remove unnecessary logger fields
-#     for k, v in data["logger"].copy().items():
-#         if k not in ["name", "tier", "tenant", "correlation_id"]:
-#             del data["logger"][k]
-
-
-# def format_request_body(key, value):
-#     if key == "password":
-#         return "*"
-#     else:
-#         # constrain the body to 64 characters per key and convert to string
-#         return str(value)[:64]
-
-
-# class RequestLogFormatter(JSONFormatter):
-#     log_tag = "request"
-
-#     def format(self, record):
-#         data = self.get_formatted_data(record)
-#         trim_logger(data)
-
-#         try:
-#             data["method"] = request.method
-#             data["url"] = request.url
-#             data["remote_addr"] = request.remote_addr
-#         except Exception:
-#             pass
-
-#         data["endpoint"] = get_clean_path_from_url(request.url)
-
-#         request_body = None
-#         try:
-#             if request.json:
-#                 request_body = {key: format_request_body(key, value)
-#                                 for key, value in request.json.items()}
-#             else:
-#                 request_body = request.data
-#         except Exception:
-#             pass
-
-#         if request_body:
-#             data["request_body"] = request_body
-
-#         try:
-#             data.update(getattr(record, "extra", {}))
-#         except Exception:
-#             pass
-
-#         if data.get('log_level') == 1:
-#             data = {
-#                 'timestamp': data['timestamp'],
-#                 'tenant': data['tenant'],
-#                 'method': data['method'],
-#                 'endpoint': data['endpoint']
-#                 }
-
-#         return self.json_format(data)
-
-
 # Calling 'logsetup' more than once may result in multiple handlers emitting
 # multiple log events for a single log call. Flagging it is a simple fix.
 _setup_done = False
-
-
-# class StreamFormatter(logging.Formatter):
-#     """
-#     The stream formatter automatically grab the record's extra field
-#     and append its content to the log message
-#     """
-#     def format(self, record):
-#         message = super(StreamFormatter, self).format(record)
-#         if hasattr(record, "extra"):
-#             message += " | {}".format(record.extra)
-#         return message
 
 
 def logsetup(app):
@@ -344,13 +121,13 @@ def logsetup(app):
         return
     _setup_done = True
 
-    from logstash_formatter import LogstashFormatterV1
-
     # configure logging
     handler = logging.StreamHandler(stream=sys.stdout)
     formatter = LogstashFormatterV1()
     handler.setFormatter(formatter)
     logging.basicConfig(handlers=[handler], level=logging.INFO)
+    if 'logging' in app.config:
+        logging.config.dictConfig(app.config['logging'])
 
     app.log_formatter = formatter
 
@@ -358,113 +135,12 @@ def logsetup(app):
     def _setup_logging():
         return setup_logging(app)
 
-    return
-
-    # # Special case for AWS Lambda. We skip all logging setup when running as Lambda.
-    # if 'AWS_EXECUTION_ENV' in os.environ:
-    #     return
-
-    # @app.before_request
-    # def log_before_request():
-    #     g.request_start_time = time.time()
-    #     g.request_log_level = 2
-
-    # if app.config.get("log_request", True):
-    #     @app.after_request
-    #     def log_after_request(response):
-    #         log_level = getattr(g, 'request_log_level', 2)
-    #         if log_level == 0:
-    #             return
-    #         resp_text = ""
-    #         resp_len = -1
-    #         try:
-    #             resp_len = len(response.response[0])
-    #             resp_text = response.response[0][:192]
-    #         except Exception:
-    #             pass
-    #         t = None
-    #         if hasattr(g, 'request_start_time'):
-    #             t = float("%.3f" % (time.time()-g.request_start_time))
-    #         extra = {
-    #             "response_code": response.status_code,
-    #             "response_length": resp_len,
-    #             "response_time": t,
-    #             "log_level": log_level,
-    #         }
-    #         if response.status_code >= 400:
-    #             extra["response"] = resp_text
-    #         if hasattr(g, "database"):
-    #             extra["database"] = g.database
-    #         logging.getLogger("request").info("{} {} - {}".format(request.method, request.url, response.status_code), extra=extra)
-
-    #         return response
-
-    # logging.setLoggerClass(ContextAwareLogger)
-
-    # syslog_path = '/dev/log'
-
-    # if sys.platform == 'darwin':
-    #     syslog_path = '/var/run/syslog'
-    # elif sys.platform == 'win32':
-    #     syslog_path = ('localhost', 514)
-
-    # # Install log file handler
-    # handler = SysLogHandler(address=syslog_path, facility=SysLogHandler.LOG_USER)
-    # handler.name = "serverlog"
-    # handler.setFormatter(ServerLogFormatter())
-    # logging.root.addHandler(handler)
-
-    # # Install eventLog file handler
-    # handler = SysLogHandler(address=syslog_path, facility=SysLogHandler.LOG_LOCAL0)
-    # handler.name = "eventlog"
-    # handler.setFormatter(EventLogFormatter())
-    # log = logging.getLogger("eventlog")
-    # log.propagate = False
-    # log.setLevel('INFO')
-    # log.addHandler(handler)
-
-    # # Install client file handler
-    # handler = SysLogHandler(address=syslog_path, facility=SysLogHandler.LOG_LOCAL1)
-    # handler.name = "clientlog"
-    # handler.setFormatter(ClientLogFormatter())
-    # log = logging.getLogger("clientlog")
-    # log.propagate = False
-    # log.setLevel('INFO')
-    # log.addHandler(handler)
-
-    # # request handler
-    # handler = SysLogHandler(address=syslog_path, facility=SysLogHandler.LOG_LOCAL2)
-    # handler.name = "request"
-    # handler.setFormatter(RequestLogFormatter())
-    # log = logging.getLogger("request")
-    # log.propagate = False
-    # log.addHandler(handler)
-
-    # # Apply additional 'level' and 'propagate' settings for handlers and
-    # # loggers. See https://docs.python.org/2.7/library/logging.config.html#
-    # # Example format:
-    # # "logging": {
-    # #     "version": 1,
-    # #     "incremental": true,
-    # #     "loggers": {
-    # #         "my_chatty_logger": {
-    # #             "level": "WARNING"
-    # #         }
-    # #     },
-    # #     "handlers": {
-    # #         "serverlog": {
-    # #             "level": "INFO",
-    # #         }
-    # #     }
-    # # }
-    # if 'logging' in app.config:
-    #     logging.config.dictConfig(app.config['logging'])
-
 
 def get_user_context():
     jwt_context = {}
     try:
-        fields = set(["user_id", "player_id", "roles", "jti", "user_name", "player_name", "client_id", "identity_id"])
+        fields = set(["user_id", "player_id", "roles", "jti", "user_name",
+                      "player_name", "client_id", "identity_id"])
         for k, v in current_user.items():
             if k in fields:
                 key = "{}".format(k)
@@ -478,8 +154,26 @@ def get_user_context():
 
 def get_log_defaults():
     defaults = {}
+    tenant_name = None
+    tier_name = get_tier_name()
+    remote_addr = None
 
-    defaults["build_info"] = current_app.config.get('BUILD_INFO')
+    try:
+        remote_addr = request.remote_addr
+    except Exception:
+        pass
+
+    try:
+        if hasattr(g, 'conf'):
+            tenant_name = g.conf.tenant_name['tenant_name'] if g.conf.tenant_name else '(none)'
+    except RuntimeError as e:
+        if "Working outside of application context" in repr(e):
+            pass
+        else:
+            raise
+    defaults["tenant"] = tenant_name
+    defaults["tier"] = tier_name
+    defaults["remote_addr"] = remote_addr
 
     jwt_context = get_user_context()
 
@@ -500,6 +194,7 @@ def get_log_defaults():
         "remote_addr": request.remote_addr,
         "path": request.path,
         "user_agent": request.headers.get('User-Agent'),
+        "endpoint": get_clean_path_from_url(request.url)
     }
     defaults["request"].update(request.view_args or {})
     return defaults
@@ -527,14 +222,3 @@ def setup_logging(app):
 
 def drift_init_extension(app, **kwargs):
     logsetup(app)
-
-
-def request_log_level(level):
-    def wrapper(fn):
-        @wraps(fn)
-        def decorated(*args, **kwargs):
-            g.request_log_level = int(level)
-            return fn(*args, **kwargs)
-
-        return decorated
-    return wrapper
